b76d9cd174747321f16d2794181cc8ef2692a43c
Caio Ferreira <caio.ferreira1914@gmail.com>
2026-03-20 14:21:16 -0300

    feat(security): implementa geração de token JWT e filtro de autenticação
    - Adicionado TokenService para criação e validação de tokens.
    - Implementado SecurityFilter para interceptar e autenticar requisições.
    - UserRepository atualizado para suporte às consultas de segurança.

---
+++ src/main/java/com/quantumbanking/infra/security/SecurityFilter.java
@@ -0,0 +1,52 @@
+package com.quantumbanking.infra.security;
+
+import com.quantumbanking.modules.shared.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final UserRepository userRepository;
+    private final TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        var tokenJWT = recoverToken(request);
+
+        if (tokenJWT != null) {
+            var cpf = tokenService.getSubject(tokenJWT);
+            var user = userRepository.findByCpf(cpf)
+                    .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+
+        var authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            return authorizationHeader.replace("Bearer ", "");
+        }
+        return null;
+    }
+}
--- src/main/java/com/quantumbanking/modules/shared/repository/UserRepository.java
+++ src/main/java/com/quantumbanking/modules/shared/repository/UserRepository.java
@@ -2,7 +2,12 @@ package com.quantumbanking.modules.shared.repository;
 
 import com.quantumbanking.modules.shared.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface  UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByCpf(@Param("cpf")String cpf);
 
 }
