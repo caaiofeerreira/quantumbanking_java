09fef711230c7d09b8c6c230b508e694bd33275e
Caio <caio.ferreira1914@gmail.com>
2026-05-02 16:00:03 -0300

    feat(transaction): integra TransactionFactory ao TransactionService

--- src/main/java/com/quantumbanking/modules/transaction/service/TransactionService.java
+++ src/main/java/com/quantumbanking/modules/transaction/service/TransactionService.java
@@ -12,6 +12,7 @@ import com.quantumbanking.modules.shared.service.UserService;
 import com.quantumbanking.modules.transaction.domain.Transaction;
 import com.quantumbanking.modules.transaction.domain.TransactionType;
 import com.quantumbanking.modules.transaction.dto.*;
+import com.quantumbanking.modules.transaction.factory.TransactionFactory;
 import com.quantumbanking.modules.transaction.mapper.TransactionMapper;
 import com.quantumbanking.modules.transaction.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
@@ -31,6 +32,8 @@ public class TransactionService {
     private final AccountRepository accountRepository;
     private final TransactionRepository transactionRepository;
     private final PixKeyRepository pixKeyRepository;
+
+    private final TransactionFactory transactionFactory;
 
     private final TransactionMapper transactionMapper;
 
@@ -53,15 +56,12 @@ public class TransactionService {
 
         Set<Long> usersToInvalidate = Set.of(user.getId());
 
-        Transaction transaction = new Transaction();
-        transaction.setAccountDestiny(account);
-        transaction.setAmount(requestDTO.amount());
-        transaction.setType(TransactionType.DEPOSIT);
-        transaction.setDescription(
-                (requestDTO.description() != null && !requestDTO.description().isBlank())
-                        ? requestDTO.description().trim()
-                        : null
-        );
+        Transaction transaction = transactionFactory
+                .createDeposit(
+                        account,
+                        requestDTO.amount(),
+                        requestDTO.description()
+                );
 
         account.credit(requestDTO.amount());
 
@@ -85,15 +85,12 @@ public class TransactionService {
 
         Set<Long> usersToInvalidate = Set.of(user.getId());
 
-        Transaction transaction = new Transaction();
-        transaction.setAccountOrigin(account);
-        transaction.setAmount(requestDTO.amount());
-        transaction.setType(TransactionType.WITHDRAWAL);
-        transaction.setDescription(
-                (requestDTO.description() != null && !requestDTO.description().isBlank())
-                        ? requestDTO.description().trim()
-                        : null
-        );
+        Transaction transaction = transactionFactory
+                .createWithdrawal(
+                        account,
+                        requestDTO.amount(),
+                        requestDTO.description()
+                );
 
         account.debit(requestDTO.amount());
 
@@ -130,19 +127,14 @@ public class TransactionService {
         usersToInvalidate.add(user.getId());
         usersToInvalidate.add(accountDestiny.getClient().getId());
 
-        Transaction transaction = new Transaction();
-        transaction.setAccountOrigin(account);
-        transaction.setOriginName(account.getClient().getName());
-        transaction.setAccountDestiny(accountDestiny);
-        transaction.setDestinyName(accountDestiny.getClient().getName());
-        transaction.setAmount(requestDTO.amount());
-        transaction.setDestinyAgency(requestDTO.agencyNumber());
-        transaction.setType(TransactionType.INTERNAL_TRANSFER);
-        transaction.setDescription(
-                (requestDTO.description() != null && !requestDTO.description().isBlank())
-                        ? requestDTO.description().trim()
-                        : null
-        );
+        Transaction transaction = transactionFactory
+                .createInternalTransfer(
+                        account,
+                        accountDestiny,
+                        requestDTO.agencyNumber(),
+                        requestDTO.amount(),
+                        requestDTO.description()
+                );
 
         account.debit(requestDTO.amount());
         accountDestiny.credit(requestDTO.amount());
@@ -173,21 +165,17 @@ public class TransactionService {
 
         Set<Long> usersToInvalidate = Set.of(user.getId());
 
-        Transaction transaction = new Transaction();
-        transaction.setAccountOrigin(account);
-        transaction.setOriginName(account.getClient().getName());
-        transaction.setDestinyAccount(requestDTO.destinyAccount());
-        transaction.setDestinyName(requestDTO.destinyName());
-        transaction.setDestinyAgency(requestDTO.destinyAgency());
-        transaction.setBankCode(requestDTO.bankCode());
-        transaction.setDestinyDocument(requestDTO.destinyDocument());
-        transaction.setAmount(requestDTO.amount());
-        transaction.setType(TransactionType.EXTERNAL_TRANSFER);
-        transaction.setDescription(
-                (requestDTO.description() != null && !requestDTO.description().isBlank())
-                        ? requestDTO.description().trim()
-                        : null
-        );
+        Transaction transaction = transactionFactory
+                .createExternalTransfer(
+                        account,
+                        requestDTO.destinyAccount(),
+                        requestDTO.destinyName(),
+                        requestDTO.destinyAgency(),
+                        requestDTO.bankCode(),
+                        requestDTO.destinyDocument(),
+                        requestDTO.amount(),
+                        requestDTO.description()
+                );
 
         account.debit(requestDTO.amount());
 
@@ -202,49 +190,36 @@ public class TransactionService {
     @Transactional
     public PixTransactionResponseDTO executePixTransaction(User user, PixTransactionRequestDTO requestDTO) {
 
-        Account account = userService
-                .getAuthenticatedUserAccount(user.getId());
+        Account originAccount = userService.getAuthenticatedUserAccount(user.getId());
 
-        if (account.getStatus() != AccountStatus.ACTIVE) {
-            throw new TransactionNotAuthorizedException("Conta não está ativa.");
+        Optional<PixKey> pixKey = pixKeyRepository.findByKey(requestDTO.key());
+        Account destinyAccount = pixKey.map(PixKey::getAccount).orElse(null);
+
+        if (destinyAccount != null && originAccount.getId().equals(destinyAccount.getId())) {
+            throw new TransactionNotAuthorizedException("Não é possível fazer Pix para a própria conta.");
         }
+
+        Transaction transaction = transactionFactory
+                .createPix(
+                        originAccount,
+                        requestDTO.amount(),
+                        requestDTO.description(),
+                        requestDTO.key(),
+                        destinyAccount
+                );
+
+        originAccount.debit(requestDTO.amount());
 
         Set<Long> usersToInvalidate = new HashSet<>();
         usersToInvalidate.add(user.getId());
 
-        Transaction transaction = new Transaction();
-        transaction.setAccountOrigin(account);
-        transaction.setOriginName(account.getClient().getName());
-        transaction.setAmount(requestDTO.amount());
-        transaction.setType(TransactionType.PIX);
-        transaction.setDescription(
-                (requestDTO.description() != null && !requestDTO.description().isBlank())
-                        ? requestDTO.description().trim()
-                        : null
-        );
-
-        Optional<PixKey> pixKey = pixKeyRepository.findByKey(requestDTO.key());
-
-        if (pixKey.isPresent()) {
-            Account accountDestiny = pixKey.get().getAccount();
-
-            if (account.getId().equals(accountDestiny.getId())) {
-                throw new TransactionNotAuthorizedException("Não é possível fazer Pix para a própria conta.");
-            }
-
-            accountDestiny.credit(requestDTO.amount());
-            transaction.setAccountDestiny(accountDestiny);
-            transaction.setDestinyName(accountDestiny.getClient().getName());
-            usersToInvalidate.add(accountDestiny.getClient().getId());
-
-            accountRepository.save(accountDestiny);
-        } else {
-            transaction.setDestinyDocument(requestDTO.key());
+        if (destinyAccount != null) {
+            destinyAccount.credit(requestDTO.amount());
+            accountRepository.save(destinyAccount);
+            usersToInvalidate.add(destinyAccount.getClient().getId());
         }
 
-        account.debit(requestDTO.amount());
-
-        accountRepository.save(account);
+        accountRepository.save(originAccount);
         transactionRepository.save(transaction);
 
         applicationEventPublisher.publishEvent(new TransactionCompletedEvent(usersToInvalidate));
