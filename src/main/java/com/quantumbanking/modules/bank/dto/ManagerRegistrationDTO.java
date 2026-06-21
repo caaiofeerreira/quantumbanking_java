186d2de29e5d28f88281ab52f9e71672df0649b6
Caio <caio.ferreira1914@gmail.com>
2026-06-21 00:50:48 -0300

    refactor: ajustando o ManagerRegistrationDTO com mensagens em campos obrigatorios

--- src/main/java/com/quantumbanking/modules/bank/dto/ManagerRegistrationDTO.java
+++ src/main/java/com/quantumbanking/modules/bank/dto/ManagerRegistrationDTO.java
@@ -1,17 +1,29 @@
 package com.quantumbanking.modules.bank.dto;
 
-import com.quantumbanking.modules.shared.domain.address.Address;
 import com.quantumbanking.modules.shared.dto.AddressRequestDTO;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record ManagerRegistrationDTO(@NotBlank String name,
-                                     @NotBlank String cpf,
-                                     @NotBlank String phone,
-                                     @Email String email,
-                                     @NotBlank String password,
-                                     @NotNull AddressRequestDTO address,
-                                     @NotBlank String agencyNumber) {
+public record ManagerRegistrationDTO(@NotBlank(message = "O nome é obrigatório.")
+                                     String name,
 
+                                     @NotBlank(message = "O CPF é obrigatório.")
+                                     String cpf,
+
+                                     @NotBlank(message = "O telefone é obrigatório.")
+                                     String phone,
+
+                                     @Email(message = "O e-mail informado é inválido.")
+                                     String email,
+
+                                     @NotBlank(message = "A senha é obrigatória.")
+                                     String password,
+
+                                     @NotNull(message = "Os dados do endereço são obrigatórios.")
+                                     @Valid AddressRequestDTO address,
+
+                                     @NotBlank(message = "O número da agência é obrigatório.")
+                                     String agencyNumber) {
 }
